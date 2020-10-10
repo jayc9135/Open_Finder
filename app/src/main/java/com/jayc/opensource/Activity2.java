b3e9efa8200c27844634ecab61f0687648adf058
jaychaudhari021@gmail.com <jAYGOOGLE@9135>
2020-10-10 23:31:08 +0530

    v1.1.0

--- app/src/main/java/com/jayc/opensource/Activity2.java
+++ app/src/main/java/com/jayc/opensource/Activity2.java
@@ -1,33 +1,48 @@
 package com.jayc.opensource;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button searchBtn;
-    private FloatingActionButton addBtn;
-    private EditText searchBar;
-    private String toSearch;
     private MaterialCardView card1;
     private RecyclerView contactsView;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    Toolbar toolbar;
+    private SearchView mainSearchView;
+
 
     public void openAddActivity(){
         Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    public void openCreditsActivity(){
+        Intent intent = new Intent(this, Credits.class);
         startActivity(intent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
@@ -43,35 +58,36 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        searchBar = findViewById(R.id.searchBar);
-        searchBtn = findViewById(R.id.searchBtn);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        mainSearchView = findViewById(R.id.mainSearchView);
+        mainSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View v) {
-                toSearch = searchBar.getText().toString();
-                Toast.makeText(Activity2.this, toSearch + " will be searched", Toast.LENGTH_SHORT).show();
+            public boolean onQueryTextSubmit(String query) {
+                String s =  mainSearchView.getQuery().toString();
+                Toast.makeText(Activity2.this, s+" will be searched", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                String s =  mainSearchView.getQuery().toString();
+                Toast.makeText(Activity2.this, s+" will be searched", Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
 
-        addBtn = findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAddActivity();
-            }
-        });
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navView = findViewById(R.id.navView);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navView.setNavigationItemSelectedListener(this);
 
 
-//        card1 = findViewById(R.id.card1);
-//        card1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openPhotoEditingActivity();
-//            }
-//        });
 
         contactsView = findViewById(R.id.contactsView);
-
         ArrayList<Card> cards = new ArrayList<Card>();
         cards.add(new Card("PHOTO EDITING", "name1@gmail.com", "https://pngimg.com/uploads/photoshop/photoshop_PNG64.png"));
         cards.add(new Card("BROWSER", "name2@gmail.com", "https://raw.githubusercontent.com/alrra/browser-logos/master/src/main-desktop-browser-logos.png"));
@@ -83,15 +99,33 @@ public class Activity2 extends AppCompatActivity {
         cards.add(new Card("MEDIA PLAYERS", "name8@gmail.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/VLC_Icon.svg/530px-VLC_Icon.svg.png"));
         cards.add(new Card("OFFICE SOFTWARE", "name9@gmail.com", "https://img2.pngio.com/excel-logo-png-download-1600763-free-transparent-microsoft-microsoft-office-2016-png-900_440.jpg"));
         cards.add(new Card("PROGRAMMING LANGUAGE", "name10@gmail.com", "https://www.sandeepkumar.co.in/assets/img/blog/top-5-programming-languages.png"));
-
         CardsRecViewAdapter adapter = new CardsRecViewAdapter(this);
         adapter.setCards(cards);
-
         contactsView.setAdapter(adapter);
         contactsView.setLayoutManager(new GridLayoutManager(this, 2));
 
     }
 
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-
+        switch(item.getItemId()){
+            case R.id.addData:
+                openAddActivity();
+                break;
+            case R.id.credits:
+                openCreditsActivity();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
---
+++ app/src/main/java/com/jayc/opensource/Credits.java
@@ -0,0 +1,33 @@
+package com.jayc.opensource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Credits extends AppCompatActivity {
+
+    private Button backBtn;
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+}
